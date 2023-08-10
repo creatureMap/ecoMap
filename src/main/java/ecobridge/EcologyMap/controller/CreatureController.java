6f9be39d93f0614dbfd0b604c844b52c8d9fcb2c
MIN SEON IK <alstjsdlr0321@korea.ac.kr>
2023-08-10 22:29:14 +0900

    전체 list 마커찍기 api - 1 commit
    - CreatureController.java 작성
    - Creature_location.java 내 빌더 패턴 생성
    - CreatureDTO.java 생성
    - CreatureLocationRepository.java 인터페이스 생성
    - CreatureRepository.java 인터페이스
    생성
    - CreatureService.java 재작성
    - CreatureControllerTest.java 작성

---
+++ src/main/java/ecobridge/EcologyMap/controller/CreatureController.java
@@ -0,0 +1,29 @@
+package ecobridge.EcologyMap.controller;
+
+import ecobridge.EcologyMap.dto.CreatureDTO;
+import ecobridge.EcologyMap.service.CreatureService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController //HTTP Response Body 에 객체 데이터를 JSON 형싟으로 변환하는 컨트롤러
+@RequestMapping("/api")  // '/api' 로 시작하는 URL 을 처리한다는 의미
+public class CreatureController {
+    private final CreatureService creatureService;
+
+    @Autowired //생성자는  CreatureService 객체를 주입받는다 - 조히 메서드 사용 가능
+    public CreatureController(CreatureService creatureService) {
+        this.creatureService = creatureService;
+    }
+
+    @GetMapping("/creatures")
+    public ResponseEntity<List<CreatureDTO>> getAllCreatureLocations() {
+        List<CreatureDTO> creatureDTOs = creatureService.getAllCreatureLocations(); //CreatureService 클래스의 메서드를 호출하여 모든 생물의 위치 정보를 조회
+        return new ResponseEntity<>(creatureDTOs, HttpStatus.OK); //반환된 'CreatureDTO' 객체 리스트를 'CreatureDTOs' 에 저장.
+    }
+}
--- src/main/java/ecobridge/EcologyMap/repository/CreatureRepository.java
+++ src/main/java/ecobridge/EcologyMap/repository/CreatureRepository.java
@@ -1,16 +1,12 @@
-//package ecobridge.EcologyMap.repository;
-//
-//import ecobridge.EcologyMap.domain.Creature;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//public interface CreatureRepository extends JpaRepository<Creature, Long> {
-//    static Optional<Creature> findById(Long creature_id);
-//    Optional<Creature> findByName(String creature_name);
-//
-//    static List<Creature> findAll();
-//
-//}
+package ecobridge.EcologyMap.repository;
+
+import ecobridge.EcologyMap.domain.Creature;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CreatureRepository extends JpaRepository<Creature, Long> {
+    //'Creature' 엔티티와 상호작용하기 위한 메서드 포함
+    // 상호작용?
+    // - 데이터베이스 와 애플리케이션 내의 엔티티(데이터 객체) 간의 데이터 처리 작업
+    // - CRUD
+    // - save(), findById(), findAll(). deleteById(). delete() ...
+}
