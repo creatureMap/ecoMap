412df73a47f3bd9a06808ab3952bd879d2bb8506
김의찬 <128672362+euichan0927@users.noreply.github.com>
2023-08-18 21:21:30 +0900

    MainCategory 지도 마커 api 완료 commit

--- src/main/java/ecobridge/EcologyMap/repository/CreatureLocationRepository.java
+++ src/main/java/ecobridge/EcologyMap/repository/CreatureLocationRepository.java
@@ -10,5 +10,7 @@ import java.util.List;
 public interface CreatureLocationRepository extends JpaRepository<CreatureLocation, Long> {
     //'Creature_location' 엔티티와 상호작용하기 위한 메서드 포함. JpaRepository에서 제공하는 여러 메서드를 활용할 수 있음.
 
+
+
 }
 
--- src/main/java/ecobridge/EcologyMap/service/CreatureService.java
+++ src/main/java/ecobridge/EcologyMap/service/CreatureService.java
@@ -6,61 +6,27 @@ import ecobridge.EcologyMap.dto.CreatureDTO;
 
 import ecobridge.EcologyMap.repository.CreatureLocationRepository;
 import ecobridge.EcologyMap.repository.CreatureRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
-
-
 import java.util.ArrayList;
 import java.util.List;
-
-
-
+import java.util.stream.Collectors;
 
 
 @Service //빈으로 등록
+@RequiredArgsConstructor
 public class CreatureService {
 
-    private final CreatureRepository creatureRepository;
     private final CreatureLocationRepository creatureLocationRepository;
-
-    @Autowired
-    public CreatureService(CreatureRepository creatureRepository, CreatureLocationRepository creatureLocationRepository) {
-        this.creatureRepository = creatureRepository;
-        this.creatureLocationRepository = creatureLocationRepository;
-    }
-
-
     /*
      * 전체 list 마커 찍기 -  DB 에서 생물의 위치 정보를 가져와 'CreatureDTO' 객체들을 생성하여 반환하는 메서드
      */
     public List<CreatureDTO> getAllCreatureLocationsWithId() {
         //findAll() 호출 -> DB 에서 모든 'Creature_location' 엔티티를 가져오기
         List<CreatureLocation> locations = creatureLocationRepository.findAll();
-        List<CreatureDTO> creatureDTOs = new ArrayList<>();
+        // locations를 creatureDTO로 변환
+        List<CreatureDTO> creatureDTOs = locations.stream().map(CreatureDTO::of).collect(Collectors.toList());
 
-        //가져온 위치 정보(Creature_location 엔티티 = locations)들을 순회하면서
-        for (CreatureLocation location : locations) {
-            Creature creature = location.getCreature(); //생물 정보를 바로 가져옵니다
-
-            //해당 위치에 연관된 생물 정보가 있다면, 생물들의 위치정보를 dto 에 저장
-            if (creature != null) {
-                CreatureDTO dto = new CreatureDTO();  //'CreatureDTO' 객체 생성
-
-                dto.setCreatureLatitude(location.getCreatureLatitude()); //위도 정보 설정
-                dto.setCreatureLongitude(location.getCreatureLongitude()); //경도 정보 설정
-
-                dto.setCreatureId(creature.getCreatureId()); //생물의 ID 설정
-                dto.setCreatureLatitude(location.getCreatureLatitude()); //위도 정보 설정
-                dto.setCreatureLongitude(location.getCreatureLongitude()); //경도 정보 설정
-
-
-
-                creatureDTOs.add(dto); //위치 정보 설정 후 'creatureDTOs' 리스트에 추가.
-            }
-        }
-
-        //모든 위치 정보에 대해 반복 수행 후, 생성된 'creatureDTOs' 리스트 반환.
         return creatureDTOs;
     }
 
@@ -72,27 +38,15 @@ public class CreatureService {
     }
 
 
-
     public List<CreatureDTO> getCategoryCreatureLocations(Long mainCategoryId) {
-            List<CreatureLocation> locations = creatureLocationRepository.findAll();
-            List<CreatureDTO> creatureDTOs = new ArrayList<>();
+        List<CreatureLocation> locations = creatureLocationRepository.findAll();
+        locations = locations.stream().filter(location->
+                location.getCreature().getMainCategory().getMainCategoryId().equals(mainCategoryId)).collect(Collectors.toList());
+        List<CreatureDTO> creatureDTOs = locations.stream().map(CreatureDTO::of).collect(Collectors.toList());
 
-            for (CreatureLocation location : locations) {
-                Creature creature = location.getCreature();
-                if (creature != null && creature.getMainCategory().getMain_category_id().equals(mainCategoryId)) {
-                    CreatureDTO dto = new CreatureDTO();
+        return creatureDTOs;
+    }
 
-                    dto.setCreatureId(creature.getCreatureId());
-                    dto.setCreatureLatitude(location.getCreatureLatitude());
-                    dto.setCreatureLongitude(location.getCreatureLongitude());
-                    dto.setLocationId(location.getLocationId());
-
-                    creatureDTOs.add(dto);
-                }
-            }
-
-            return creatureDTOs;
-        }
 
 }
 
