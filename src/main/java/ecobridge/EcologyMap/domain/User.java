4f8b18ed7ea6c491a232b6b99f9b80ddaf1dae96
김의찬 <128672362+euichan0927@users.noreply.github.com>
2023-08-25 22:26:55 +0900

    User 주석 해제 commit

--- src/main/java/ecobridge/EcologyMap/domain/User.java
+++ src/main/java/ecobridge/EcologyMap/domain/User.java
@@ -1,83 +1,83 @@
-//package ecobridge.EcologyMap.domain;
-//
-//import jakarta.persistence.*;
-//import lombok.AccessLevel;
-//import lombok.Builder;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.core.authority.SimpleGrantedAuthority;
-//import org.springframework.security.core.userdetails.UserDetails;
-//
-//import java.util.Collection;
-//import java.util.List;
-//
-//@Table(name= "user")
-//@Entity
-//@Getter
-//@NoArgsConstructor(access = AccessLevel.PROTECTED)
-//public class User implements UserDetails {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    @Column(name = "id", updatable = false)
-//    private Long id;
-//
-//    @Column(name = "username", unique = true, nullable = false)
-//    private String username;
-//
-//    @Column(name = "password", nullable = false)
-//    private String password;
-//
-//    @Column(name="email", nullable = false)
-//    private String email;
-//
-//    @Column(name = "refresh_token", nullable = false)
-//    private String refreshToken;
-//
-//    public User refreshToken(String refreshToken) {
-//        this.refreshToken = refreshToken;
-//        return this;
-//    }
-//
-//    @Builder
-//    public User(String username, String password, String refreshToken, String auth) {
-//        this.username = username;
-//        this.password = password;
-//        this.refreshToken = refreshToken;
-//    }
-//
-//    @Override //권한 반환
-//    public Collection<? extends GrantedAuthority> getAuthorities() {
-//        return List.of(new SimpleGrantedAuthority("user"));
-//    }
-//
-//    @Override
-//    public String getUsername() {
-//        return username;
-//    }
-//
-//    @Override
-//    public String getPassword() {
-//        return password;
-//    }
-//
-//    @Override //계정 만료 여부 확인
-//    public boolean isAccountNonExpired() {
-//        return false; //false는 만료
-//    }
-//
-//    @Override //계정 잠금 여부 확인
-//    public boolean isAccountNonLocked() {
-//        return false; //false는 잠금
-//    }
-//
-//    @Override //패스워드 만료 여부 확인
-//    public boolean isCredentialsNonExpired() {
-//        return false; //false는 만료
-//    }
-//
-//    @Override //계정 사용 가능 여부 확인
-//    public boolean isEnabled() {
-//        return false; //false는 사용 불가
-//    }
-//}
+package ecobridge.EcologyMap.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Table(name= "user")
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "username", unique = true, nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name="email", nullable = false)
+    private String email;
+
+    @Column(name = "refresh_token", nullable = false)
+    private String refreshToken;
+
+    public User refreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+        return this;
+    }
+
+    @Builder
+    public User(String username, String password, String refreshToken, String auth) {
+        this.username = username;
+        this.password = password;
+        this.refreshToken = refreshToken;
+    }
+
+    @Override //권한 반환
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("user"));
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override //계정 만료 여부 확인
+    public boolean isAccountNonExpired() {
+        return false; //false는 만료
+    }
+
+    @Override //계정 잠금 여부 확인
+    public boolean isAccountNonLocked() {
+        return false; //false는 잠금
+    }
+
+    @Override //패스워드 만료 여부 확인
+    public boolean isCredentialsNonExpired() {
+        return false; //false는 만료
+    }
+
+    @Override //계정 사용 가능 여부 확인
+    public boolean isEnabled() {
+        return false; //false는 사용 불가
+    }
+}
--- src/main/java/ecobridge/EcologyMap/repository/UserRepository.java
+++ src/main/java/ecobridge/EcologyMap/repository/UserRepository.java
@@ -1,10 +1,10 @@
-//package ecobridge.EcologyMap.repository;
-//
-//import ecobridge.EcologyMap.domain.User;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import java.util.Optional;
-//
-//public interface UserRepository extends JpaRepository<User, Long> {
-//    Optional<User> findByUsername(String username);
-//}
+package ecobridge.EcologyMap.repository;
+
+import ecobridge.EcologyMap.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
