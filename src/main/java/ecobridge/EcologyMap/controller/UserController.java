f5b146de1eed365b5f627ab3526cd815150c927c
Your Name <psoyeon0127@naver.com>
2023-08-25 15:14:04 +0900

    UserController error

--- src/main/java/ecobridge/EcologyMap/controller/UserController.java
+++ src/main/java/ecobridge/EcologyMap/controller/UserController.java
@@ -3,12 +3,14 @@ package ecobridge.EcologyMap.controller;
 //회원가입 및 로그인
 
 import ecobridge.EcologyMap.dto.AddUserRequest;
+import ecobridge.EcologyMap.service.TokenService;
 import ecobridge.EcologyMap.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
@@ -22,10 +24,20 @@ import java.util.List;
 public class UserController {
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
     private final UserService userService;
+    private final TokenService tokenService;
 
     @GetMapping("/login")
     public String login() {
         return "login";
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<AddUserRequest> createNewAccessToken(
+            @RequestBody AddUserRequest request) {
+        String newAccessToken = tokenService.createNewAccessToken(request.getRefreshToken());
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new AddUserRequest());
     }
 
     @GetMapping("/logout")
--- src/main/java/ecobridge/EcologyMap/dto/AddUserRequest.java
+++ src/main/java/ecobridge/EcologyMap/dto/AddUserRequest.java
@@ -1,11 +1,15 @@
 package ecobridge.EcologyMap.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class AddUserRequest {
     private String username;
     private String password;
+    private String refreshToken;
+    private String accessToken;
 }
