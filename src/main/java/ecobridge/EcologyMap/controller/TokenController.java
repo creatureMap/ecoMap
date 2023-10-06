83cfeacdfc9bb0476031ccb3c0d426870002d060
MIN SEON IK <alstjsdlr0321@korea.ac.kr>
2023-10-06 15:13:45 +0900

    챗봇 - 3줄요약

--- src/main/java/ecobridge/EcologyMap/controller/TokenController.java
+++ src/main/java/ecobridge/EcologyMap/controller/TokenController.java
@@ -1,26 +1,26 @@
-//package ecobridge.EcologyMap.controller;
-//
-//import ecobridge.EcologyMap.dto.CreateAccessTokenRequest;
-//import ecobridge.EcologyMap.dto.CreateAccessTokenResponse;
-//import ecobridge.EcologyMap.service.TokenService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RequiredArgsConstructor
-//@RestController
-//public class TokenController {
-//    private final TokenService tokenService;
-//
-//    @PostMapping("/api/token")
-//    public ResponseEntity<CreateAccessTokenResponse> createNewAccessToken
-//            (@RequestBody CreateAccessTokenRequest request) {
-//        String newAccessToken = tokenService.createNewAccessToken(request.getRefreshToken());
-//
-//        return ResponseEntity.status(HttpStatus.CREATED)
-//                .body(new CreateAccessTokenResponse(newAccessToken));
-//    }
-//}
+package ecobridge.EcologyMap.controller;
+
+import ecobridge.EcologyMap.dto.CreateAccessTokenRequest;
+import ecobridge.EcologyMap.dto.CreateAccessTokenResponse;
+import ecobridge.EcologyMap.service.TokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class TokenController {
+    private final TokenService tokenService;
+
+    @PostMapping("/api/token")
+    public ResponseEntity<CreateAccessTokenResponse> createNewAccessToken
+            (@RequestBody CreateAccessTokenRequest request) {
+        String newAccessToken = tokenService.createNewAccessToken(request.getRefreshToken());
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new CreateAccessTokenResponse(newAccessToken));
+    }
+}
--- src/main/java/ecobridge/EcologyMap/dto/CreateAccessTokenRequest.java
+++ src/main/java/ecobridge/EcologyMap/dto/CreateAccessTokenRequest.java
@@ -1,11 +1,11 @@
-//package ecobridge.EcologyMap.dto;
-//
-//import lombok.Getter;
-//import lombok.Setter;
-//
-//@Getter
-//@Setter
-//public class CreateAccessTokenRequest {
-//    private String refreshToken;
-//}
-//
+package ecobridge.EcologyMap.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateAccessTokenRequest {
+    private String refreshToken;
+}
+
