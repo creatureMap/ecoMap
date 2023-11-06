a70dfb11b6801415938890cfc9fcd9bed3862b65
박소연 <psoyeon0127@naver.com>
2023-11-06 17:58:51 +0900

    login

--- src/main/java/ecobridge/EcologyMap/service/TokenService.java
+++ src/main/java/ecobridge/EcologyMap/service/TokenService.java
@@ -17,12 +17,14 @@ public class TokenService {
 
     //리프레시 토큰 검증 및 유저 정보 조회, 새로운 엑세스 토큰 발급
     public String createNewAccessToken(String refreshToken) {
-        System.out.println(refreshToken);
         if(!tokenProvider.validToken(refreshToken)) {
             throw new IllegalArgumentException("unexpected token");
         }
 
+        // 토큰에서 유저 정보 조회
         Long userId = refreshTokenService.findByRefreshToken(refreshToken).getUserId();
+
+        // 토큰의 유저 정보를 데이터베이스에서 조회
         User user = userService.findById(userId);
 
         return tokenProvider.generateToken(user, Duration.ofHours(2));
