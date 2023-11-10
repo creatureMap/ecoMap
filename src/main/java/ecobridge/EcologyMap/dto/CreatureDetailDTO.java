40f9bf439f6553e67a1ea95d90f69ac4c6049fa1
kim946509 <kim946509@gmail.com>
2023-11-10 18:40:00 +0900

    CreatureContractionDTO 삭제 및 CreatureDetailDTO 필수정보만 가지도록 수정

--- src/main/java/ecobridge/EcologyMap/dto/CreatureDetailDTO.java
+++ src/main/java/ecobridge/EcologyMap/dto/CreatureDetailDTO.java
@@ -9,23 +9,23 @@ import lombok.*;
 public class CreatureDetailDTO {
 
     private Long creatureId;
-    private double creatureLatitude;
-    private double creatureLongitude;
-    private long locationId;
-    private Long mainCategoryId;
-    private Long detailCategoryId;
-    private Long creatureProtectionClass;
     private String creatureName;
     private String imageUrl;
-    private boolean spring;
-    private boolean summer;
-    private boolean fall;
-    private boolean winter;
     private String mainCategoryName;
     private String detailCategoryName;
-    private String locatioName;
     private String creatureInformation;
     private String creatureSummaryInformation;
+//    private double creatureLatitude;
+//    private double creatureLongitude;
+//    private long locationId;
+//    private Long mainCategoryId;
+//    private Long detailCategoryId;
+//    private Long creatureProtectionClass;
+//    private boolean spring;
+//    private boolean summer;
+//    private boolean fall;
+//    private boolean winter;
+//    private String locatioName;
 
 
 
@@ -33,20 +33,19 @@ public class CreatureDetailDTO {
     static public CreatureDetailDTO of(CreatureLocation location){
         return CreatureDetailDTO.builder()
                 .creatureId(location.getCreature().getCreatureId())
-                .creatureLatitude(location.getCreatureLatitude())
-                .creatureLongitude(location.getCreatureLongitude())
-                .locationId(location.getLocationId())
-                .mainCategoryId(location.getCreature().getMainCategory().getMainCategoryId())
                 .mainCategoryName(location.getCreature().getMainCategory().getMainCategoryName())
                 .detailCategoryName(location.getCreature().getDetailCategory().getDetailCategoryName())
-                .detailCategoryId(location.getCreature().getDetailCategory().getDetailCategoryId())
-                .creatureProtectionClass(location.getCreature().getCreatureProtectionClass())
                 .imageUrl(location.getCreature().getImageUrl())
                 .creatureName(location.getCreature().getCreatureName())
-                .locatioName(location.getLocationName())
                 .creatureInformation(location.getCreature().getCreatureInformation())
                 .creatureSummaryInformation(location.getCreature().getCreatureSummaryInformation())
-
                 .build();
+//                .creatureLatitude(location.getCreatureLatitude())
+//                .creatureLongitude(location.getCreatureLongitude())
+//                .locationId(location.getLocationId())
+//                .mainCategoryId(location.getCreature().getMainCategory().getMainCategoryId())
+//                .detailCategoryId(location.getCreature().getDetailCategory().getDetailCategoryId())
+//                .creatureProtectionClass(location.getCreature().getCreatureProtectionClass())
+//                .locatioName(location.getLocationName())
     }
 }
