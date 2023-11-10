3c022c454a1f1dde1d6f87d169091939be8c7409
김의찬 <128672362+euichan0927@users.noreply.github.com>
2023-11-10 13:39:54 +0900

    도감추가 api 완성

--- src/main/java/ecobridge/EcologyMap/controller/UserController.java
+++ src/main/java/ecobridge/EcologyMap/controller/UserController.java
@@ -3,9 +3,12 @@ package ecobridge.EcologyMap.controller;
 //회원가입 및 로그인
 
 import ecobridge.EcologyMap.config.jwt.TokenProvider;
+import ecobridge.EcologyMap.domain.BiologyEncyclopedia;
 import ecobridge.EcologyMap.domain.User;
+import ecobridge.EcologyMap.dto.BiologyEncyclopediaDTO;
 import ecobridge.EcologyMap.dto.UserDTO;
 import ecobridge.EcologyMap.repository.UserRepository;
+import ecobridge.EcologyMap.service.BiologyEncyclopediaService;
 import ecobridge.EcologyMap.service.TokenService;
 import ecobridge.EcologyMap.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,16 +16,16 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.*;
+
 import java.time.Duration;
-import java.util.Map;
-import ecobridge.EcologyMap.dto.BiologyEncyclopediaDTO;
-import ecobridge.EcologyMap.service.BiologyEncyclopediaService;
 import java.util.List;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
@@ -85,4 +88,23 @@ public class UserController {
     public List<BiologyEncyclopediaDTO> getUserCreaturesByDetailCategoryName(@PathVariable Long userId, @PathVariable String detailCategoryName){
         return biologyEncyclopediaService.getUserCreaturesByDetailCategoryName(userId,detailCategoryName);
     }
+
+    //사용자 도감에 생물을 추가하는 api
+    @PostMapping("/{userId}/Encyclopedia/{creatureId}/{correctAnswers}")
+    public ResponseEntity<Boolean> addCreatureToUser(@PathVariable Long userId, @PathVariable Long creatureId, @PathVariable int correctAnswers) {
+        if(correctAnswers >= 2) {
+
+            try {
+                BiologyEncyclopedia biology = biologyEncyclopediaService.addUserCreature(userId, creatureId);
+                return ResponseEntity.ok(biology != null);
+            } catch (Exception e) {
+                logger.error("Failed to add creature to user's encyclopedia", e);
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(false);
+            }
+        }
+        else {
+            return ResponseEntity.ok(false);
+        }
+    }
+
 }
